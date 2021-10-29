22dcf3c62e50592fb3420425bd855ddc229c70d7
kmi0817 <62174395+kmi0817@users.noreply.github.com>
2021-10-29 13:54:06 +0900

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,46 @@
+from app import app
+from flask import render_template, redirect, url_for, session, request, Response
+import paramiko
+import os.path
+
+# 로그인 정보
+root_email, root_password = "asd@asd.com", "asd"
+
+
+@app.route('/')
+@app.route('/irb')
+def irb() :
+    signin = False
+    if 'IRB_signin' in session :
+        signin = True
+    return render_template('irb.html', IRB_signin=signin)
+
+@app.route('/irb/process-signin', methods=['POST'])
+def irb_process_signin() :
+    values = request.get_json(force=True)
+    email = values['email']
+    password = values['password']
+
+    if (root_email == email and root_password == password) :
+        session['IRB_signin'] = True
+        return redirect(url_for('irb'))
+    else :
+        return '<script>alert("Check Inputs");</script>'
+
+@app.route('/irb/process-signout', methods=['POST'])
+def irb_process_signout() :
+    session.clear() # 모든 파이썬 세션 삭제
+    return "IRB Sign Out"
+
+
+
+
+@app.route('/researcher-irb')
+def researcher_irb() :
+    return render_template('researcher_irb.html')
+
+@app.route('/researcher-irb/accpet-invitation')
+def researcher_irb_accept_invitation() :
+    values = request.get_json(force=True)
+    session['IRB_inv'] = values
+    return "Researcher accepts IRB invitation"
